6e6a83107bc0ad03264beabf0eeb5acae9239f84
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-18 02:05:42 +0000

    Automatically backed up by Learn

---
+++ lib/user.rb
@@ -0,0 +1,22 @@
+class User 
+
+  def initialize(first_name,last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  def first_name
+    @first_name
+  end
+ 
+#   def first_name=(new_name)
+#     @first_name = new_name
+#   end
+
+#   def @last_name=(last_name)
+#   @last_name = last_name
+# end 
+ 
+ def last_name
+   @last_name
+ end 
+end 
